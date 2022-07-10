132112dabdc313a15a8fe3aa6870d6e422403fe9
GHhidatang <wai2333@163.com>
2022-07-10 15:01:29 +0800

    重新修改

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+ <div class="hello-world">
+  <img src="../assets/B站专栏封面3.jpg" alt="">
+ </div>
+</template>
+
+<style lang="scss" scoped>
+.hello-world {
+  width: 100%;
+  height: 100%;
+}
+</style>
